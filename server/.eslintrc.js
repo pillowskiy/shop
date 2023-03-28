7b41d3d2e6c112ad998532982a0bbb964a7288ed
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-03-28 19:21:43 +0300

    bin

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -17,6 +17,9 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    "prettier/prettier": ["error",{
+      "endOfLine": "auto"}
+    ],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
