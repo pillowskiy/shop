28f9fff5d2295617c62ad6b8f4c42e2dda8c1a98
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-03-30 20:07:31 +0300

    category entity

--- server/src/category/category.controller.ts
+++ server/src/category/category.controller.ts
@@ -1,7 +1,56 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 import { CategoryService } from './category.service';
+import { CategoryDto } from './dto/category.dto';
 
-@Controller('category')
+@Controller('categories')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
+
+  @Get()
+  public getCategories() {
+    return this.categoryService.getAll();
+  }
+  @Get('/id/:id')
+  public getCategoryById(@Param('id') id: string) {
+    return this.categoryService.getCategoryByQuery({ id: +id });
+  }
+  @Get('/slug/:slug')
+  public getCategoryBySlug(@Param('slug') slug: string) {
+    return this.categoryService.getCategoryByQuery({ slug });
+  }
+  @Post('create')
+  @Auth()
+  @HttpCode(200)
+  public createCategory() {
+    return this.categoryService.create();
+  }
+  @Delete(':id')
+  @Auth()
+  @HttpCode(200)
+  public deleteCategory(@Param('id') id: string) {
+    return this.categoryService.delete(+id);
+  }
+
+  @Put(':id')
+  @Auth()
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  public updateCategory(
+    @Param('id') id: string,
+    @Body() category: CategoryDto,
+  ) {
+    return this.categoryService.update(+id, category);
+  }
 }
--- server/src/category/category.service.ts
+++ server/src/category/category.service.ts
@@ -1,21 +1,49 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { NotFoundException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
+import { CategoryDto } from './dto/category.dto';
 import { categorySelect } from './prisma.partials';
+import slugify from 'src/utils/slugify';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class CategoryService {
   constructor(private readonly prisma: PrismaService) {}
 
-  public async getCategoryById(categoryId: number) {
+  public async getCategoryByQuery(query: Prisma.CategoryWhereUniqueInput) {
     const category = await this.prisma.category.findUnique({
-      where: { id: categoryId },
+      where: query,
       select: categorySelect,
     });
 
     if (!category) {
-      throw new BadRequestException('Category not found');
+      throw new NotFoundException('Category not found');
     }
 
     return category;
+  }
+  public async delete(categoryId: number) {
+    await this.getCategoryByQuery({ id: categoryId });
+    return this.prisma.category.delete({
+      where: { id: categoryId },
+      select: categorySelect,
+    });
+  }
+  public getAll() {
+    return this.prisma.category.findMany({
+      select: categorySelect,
+    });
+  }
+  public create() {
+    return this.prisma.category.create({
+      data: { name: '', slug: '' },
+      select: categorySelect,
+    });
+  }
+  public update(categoryId: number, { name }: CategoryDto) {
+    return this.prisma.category.update({
+      where: { id: categoryId },
+      select: categorySelect,
+      data: { name, slug: slugify(name) },
+    });
   }
 }
