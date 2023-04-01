95a94edfe0b86cd46506a40a9f4c1e66f4dde0b7
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-04-01 23:44:40 +0300

    access modifiers

--- server/src/auth/dto/jwt.dto.ts
+++ server/src/auth/dto/jwt.dto.ts
@@ -2,5 +2,5 @@ import { IsString } from 'class-validator';
 
 export class JwtRefreshTokenDto {
   @IsString()
-  refreshToken: string;
+  public refreshToken: string;
 }
--- server/src/category/dto/category.dto.ts
+++ server/src/category/dto/category.dto.ts
@@ -2,5 +2,5 @@ import { IsString } from 'class-validator';
 
 export class CategoryDto {
   @IsString()
-  name: string;
+  public name: string;
 }
