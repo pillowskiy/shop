9e87281116d504b2e667d7e565353b689ab42644
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-04-25 20:07:17 +0300

    swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,16 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Shop')
+    .setDescription("Pillow's shop documentation")
+    .setVersion('1.0.0')
+    .addTag('shop')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
   await app.listen(8080);
 }
 bootstrap();
