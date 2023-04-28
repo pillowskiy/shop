b83155d01e8207385559bdee53a47ee99353641e
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-04-28 19:39:29 +0300

    product service

---
+++ client/src/api/services/product.service.ts
@@ -0,0 +1,47 @@
+import type { AxiosResponse } from 'axios'
+import { $api } from '../api.interceptor'
+import type {
+  Filter,
+  Product,
+  ProductFullest,
+  UpdateProductData,
+} from '@/types/product.interface';
+
+export default class ProductService {
+  private static controller = 'products';
+
+  static async getAll(filterParams?: Filter)
+    : Promise<AxiosResponse<Product[]>> {
+    return $api.get<Product[]>(`${this.controller}`, { params: filterParams || {} });
+  }
+
+  static async getByValue<T extends keyof Pick<Product, 'id' | 'slug'>>(
+    type: T,
+    value: Product[T]
+  ): Promise<AxiosResponse<Product>> {
+    return $api.get<Product>(`${this.controller}/${type}/${value}`);
+  }
+
+  static async getByCategorySlug(slug: string): Promise<AxiosResponse<Product>> {
+    return $api.get<Product>(`${this.controller}/category/${slug}`);
+  }
+
+  static async getSimilar(productId: number): Promise<AxiosResponse<Product[]>> {
+    return $api.get<Product[]>(`${this.controller}/similar/${productId}`);
+  }
+
+  static async create(): Promise<AxiosResponse<number>> {
+    return $api.post<number>(`${this.controller}`);
+  }
+
+  static async update(
+    productId: number,
+    data: UpdateProductData
+  ): Promise<AxiosResponse<ProductFullest>> {
+    return $api.put<ProductFullest>(`${this.controller}/${productId}`, data);
+  }
+
+  static async delete(productId: number): Promise<AxiosResponse<number>> {
+    return $api.delete<number>(`${this.controller}/${productId}`);
+  }
+}
