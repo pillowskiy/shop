d52b20cf5af5b27522cd5fcbeada389200a1890f
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-05-02 20:32:42 +0300

    meta

--- client/next.config.js
+++ client/next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
   env: {
     SERVER_URL: process.env.API_URL,
     API_URL: process.env.API_URL,
+    APP_URL: process.env.APP_URL,
+    APP_NAME: process.env.APP_NAME,
   }
 }
 
