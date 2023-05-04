596b75eaa43fcd9d741c02623731203301ef61a1
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-05-04 22:59:10 +0300

    typified use actions hook
    I looked through a lot of articles to make a typified useActions hook, and I managed to do it, but I don't like the end result. It's all about AsyncThunkConfig, which loses typing when you change it.

--- client/src/hooks/useActions.ts
+++ client/src/hooks/useActions.ts
@@ -1,11 +1,31 @@
-import { bindActionCreators } from '@reduxjs/toolkit';
-import { useMemo } from 'react';
-import { useDispatch } from 'react-redux'
-import * as actions from '@redux/user/user.actions';
+import {
+  ActionCreator,
+  ActionCreatorsMapObject,
+  AsyncThunk,
+  bindActionCreators,
+} from "@reduxjs/toolkit";
+import { useMemo } from "react";
+import { useAppDispatch } from "@redux/store";
+import * as actions from "@redux/user/user.actions";
 
-export const useActions = () => {
-  const dispatch = useDispatch();
-  return useMemo(() => {
-    return bindActionCreators(actions, dispatch)
-  }, [dispatch])
-}
+type BoundActions<Actions extends ActionCreatorsMapObject> = {
+  [key in keyof Actions]: Actions[key] extends AsyncThunk<any, any, any>
+      ? BoundAsyncThunk<Actions[key]>
+      : Actions[key];
+};
+
+type BoundAsyncThunk<Action extends ActionCreator<any>> = (
+    ...args: Parameters<Action>
+) => ReturnType<ReturnType<Action>>;
+
+export const useActionCreatorsTyped = <
+  Actions extends ActionCreatorsMapObject = ActionCreatorsMapObject
+>(
+    actions: Actions
+): BoundActions<Actions> => {
+  const dispatch = useAppDispatch();
+
+  return useMemo(() => bindActionCreators(actions, dispatch), [dispatch]);
+};
+
+export const useUserActions = () => useActionCreatorsTyped(actions);
--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -9,22 +9,19 @@ import {
   REHYDRATE,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-
 import { userSlice } from './user/user.slice';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import {useDispatch} from "react-redux";
 
 const persistConfig = {
   key: 'root',
   whitelist: ['cart'],
   storage,
 }
-
 const rootReducer = combineReducers({
   user: userSlice.reducer,
 });
-
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
@@ -33,7 +30,8 @@ export const store = configureStore({
     },
   }),
 });
-
 export const persistor = persistStore(store);
 
-export type RootState = ReturnType<typeof rootReducer>;
+export type RootState = ReturnType<typeof rootReducer>
+export type AppDispatch = typeof store.dispatch
+export const useAppDispatch: () => AppDispatch = useDispatch;
