e28a9c11f99b7856536cdb21fc115716068831cb
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-05-08 21:10:22 +0300

    tailwind responsive padding value

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -53,6 +53,9 @@ module.exports = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      padding: {
+        custom: "var(--padding)"
+      },
       keyframes: {
         "accordion-down": {
           from: { height: 0 },
