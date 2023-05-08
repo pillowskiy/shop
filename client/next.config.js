b5997d26a1676477fc87d537a01c0232cfab6724
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-05-08 21:10:42 +0300

    next 13.4.1

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  experimental: {
+    typedRoutes: true,
+  },
   env: {
     SERVER_URL: process.env.API_URL,
     API_URL: process.env.API_URL,
