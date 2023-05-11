bf56a9e3f1e0e4eaf19e8e996d8f258ff380f589
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-05-11 18:28:09 +0300

    checking the existence of local storage

--- client/src/api/services/token.service.ts
+++ client/src/api/services/token.service.ts
@@ -1,8 +1,9 @@
 import type { AuthResponse } from "@types";
+import {getFromLocalStorage} from "@lib/utils";
 
 export default class TokenService {
   static getToken(): string {
-    return localStorage.getItem('token') || '';
+    return getFromLocalStorage('token') || '';
   }
   static setToken(data: AuthResponse): void {
     return localStorage.setItem('token', data.accessToken);
--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,14 +1,20 @@
-import { ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import {ClassValue, clsx} from "clsx"
+import {twMerge} from "tailwind-merge"
 import {isAxiosError} from "axios";
  
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+export const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs));
+};
 
-export function rejectAxios(err: unknown) {
+export const rejectAxios = (err: unknown) => {
   if (isAxiosError(err) && err.response) {
     return err.response.data;
   }
   throw err;
+}
+
+export const getFromLocalStorage = (key: string) => {
+  if (typeof window !== 'undefined') {
+    return localStorage.getItem(key);
+  }
 }
