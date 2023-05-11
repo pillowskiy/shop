f55fa1ad2ce7a743c1217bb14be6a14b2228ace4
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-05-11 18:26:06 +0300

    products ssg

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,22 +1,39 @@
 "use client"
 
-import {Anchor} from "@ui/Anchor";
 import {Header} from "@containers/header/Header";
 import {SideBar} from "@containers/aside/SideBar";
 import {OfferCarousel} from "@containers/swiper/OfferCarousel";
 import {Main} from "@containers/Main";
+import {Meta} from "@containers/Meta";
+import {Catalog} from "@containers/Catalog";
+import {GetStaticProps, NextPage} from "next";
+import {GetAllProductsResponse} from "@/types/product.interface";
+import ProductService from "@api/services/product.service";
 
-export default function Home() {
+const HomePage: NextPage<GetAllProductsResponse> = ({products, length}) => {
     return (
-        <>
+        <Meta title="Online Shop">
             <Header/>
             <SideBar/>
             <Main>
                 <OfferCarousel/>
-                <Anchor href="/products">Products</Anchor>
-                <Anchor href="/login">Login</Anchor>
-                <Anchor href="/register">Register</Anchor>
+                <Catalog products={products} length={length}/>
             </Main>
-        </>
-    )
+        </Meta>
+    );
 }
+
+export const getStaticProps: GetStaticProps<GetAllProductsResponse> = async () => {
+    const {data} = await ProductService.getAll({
+        page: 1,
+        perPage: 10,
+    });
+
+    return {
+        props: {
+            ...data,
+        }
+    };
+}
+
+export default HomePage;
