a385ebfd75906386c0fb18524dc35a3bb6ca5b6f
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-05-11 18:28:39 +0300

    remove client side rendering

--- client/src/containers/cards/product/ProductItem.tsx
+++ client/src/containers/cards/product/ProductItem.tsx
@@ -1,5 +1,5 @@
 import type {FC} from 'react';
-import type {ProductFullest} from "@types/product.interface";
+import type {ProductFullest} from "@/types/product.interface";
 import {Card, CardContent} from "@common/Card";
 import {FavoriteButton} from "./layout/FavoriteButton";
 import {ProductRating} from "./layout/ProductRating";
@@ -16,13 +16,13 @@ export const ProductItem: FC<ProductItemProps> = ({product}) => {
             <CardContent className="px-custom md:px-4 py-3 w-full">
                 <span className="text-muted uppercase text-xs">{product.category.name}</span>
                 <p className="text-lg font-bold text-foreground truncate block capitalize">{product.name}</p>
-                <div className="flex items-center">
+                <section className="flex items-center">
                     <p className="text-lg font-semibold text-black cursor-auto my-3">{product.price}$</p>
                     <del>
                         <p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
                     </del>
                     <FavoriteButton className="block ml-auto" productId={product.id}/>
-                </div>
+                </section>
                 <ProductRating product={product}/>
             </CardContent>
         </Card>
