4687398d1a6525d2b3fc0d917f3d835658c888c0
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-05-13 21:38:16 +0300

    svg import

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // https://stackoverflow.com/questions/55175445/cant-import-svg-into-next-js
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack', 'url-loader'],
+    });
+
+    return config;
+  },
   reactStrictMode: false,
   experimental: {
     typedRoutes: true,
