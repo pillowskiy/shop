bc1325228315ac94ee148e7bfdab02c0fb6efbb6
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-05-19 18:40:01 +0300

    side bar items onlick router

--- client/src/containers/aside/SideBar.tsx
+++ client/src/containers/aside/SideBar.tsx
@@ -1,7 +1,7 @@
 import type {FC} from 'react';
 import {SideBarItems} from "@containers/aside/layout/SideBarItems";
 import {SideBarItem} from "@containers/aside/layout/SideBarItem";
-import {Globe2, ShoppingCart, Heart, List} from "lucide-react";
+import {ShoppingCart, Heart, List} from "lucide-react";
 import {cn} from "@lib/utils";
 import {SideBarProfile} from "@containers/aside/layout/SideBarProfile";
 
@@ -9,14 +9,13 @@ export const SideBar: FC = () => {
     return (
         <aside className={cn(
             "fixed bottom-0 md:top-0 md:bottom-auto text-foreground bg-popover border-muted border-t shadow-md md:shadow-none",
-            "w-full md:w-20 md:pt-32 md:pb-4 md:px-0 h-14 md:h-screen z-20"
+            "w-full md:w-20 md:pt-48 md:pb-4 md:px-0 h-14 md:h-screen z-20"
         )}
         >
             <SideBarItems>
-                <SideBarItem Icon={ShoppingCart} title="Cart" />
-                <SideBarItem Icon={Heart} title="Favorites" />
-                <SideBarItem Icon={List} title="Orders" />
-                <SideBarItem Icon={Globe2} title="Language" />
+                <SideBarItem href="#" Icon={ShoppingCart} title="Cart" />
+                <SideBarItem href="/favorites" Icon={Heart} title="Favorites" />
+                <SideBarItem href="#"Icon={List} title="Orders" />
                 <SideBarProfile />
             </SideBarItems>
         </aside>
--- client/src/containers/aside/layout/SideBarItem.tsx
+++ client/src/containers/aside/layout/SideBarItem.tsx
@@ -1,10 +1,12 @@
 import type {FC, HTMLAttributes, PropsWithChildren} from "react";
 import {cn} from "@lib/utils";
 import {LucideIcon} from "lucide-react";
+import {useRouter} from "next/router";
 
 interface SideBarItemProps extends HTMLAttributes<HTMLLIElement> {
     title?: string;
     Icon: LucideIcon;
+    href: string;
 }
 
 export const SideBarItem: FC<PropsWithChildren<SideBarItemProps>> = ({
@@ -12,11 +14,15 @@ export const SideBarItem: FC<PropsWithChildren<SideBarItemProps>> = ({
     className,
     title,
     Icon,
+    href,
     ...props
 }) => {
+    const router = useRouter();
+
     return (
         <li
             className={cn("cursor-pointer p-2 hover:bg-muted rounded-lg transition-all text-center w-1/5 md:w-10/12", className)}
+            onClick={() => router.push(href)}
             {...props}
         >
             <Icon className="h-7 w-7 m-auto" />
