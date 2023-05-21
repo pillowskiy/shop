f02039d7618e63215683ae3b96b4ddbb629c68f7
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-05-21 17:56:17 +0300

    query client options

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,22 +1,28 @@
 import '@styles/globals.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import type { AppProps } from 'next/app';
-import { persistor, store } from '@redux/store';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { AuthFields } from '@/types/providers/auth-provider';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import type {AppProps} from 'next/app';
+import {persistor, store} from '@redux/store';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {AuthFields} from '@/types/providers/auth-provider';
 import {Toaster} from "@common/toast";
 
-const queryClient = new QueryClient();
-export default function App({ Component, pageProps }: AppProps & AuthFields) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-            <Toaster />
-            <Component {...pageProps}/>
-        </PersistGate>
-      </Provider>
-    </QueryClientProvider>
-  )
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+        }
+    }
+});
+export default function App({Component, pageProps}: AppProps & AuthFields) {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Provider store={store}>
+                <PersistGate loading={null} persistor={persistor}>
+                    <Toaster/>
+                    <Component {...pageProps}/>
+                </PersistGate>
+            </Provider>
+        </QueryClientProvider>
+    )
 }
