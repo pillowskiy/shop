6c0a3967fe57b1ac2ce28a5536f56242e5765f9e
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-05-21 17:54:55 +0300

    react.fragment

--- client/src/containers/cards/product/layout/ProductBreadcrumb.tsx
+++ client/src/containers/cards/product/layout/ProductBreadcrumb.tsx
@@ -1,4 +1,4 @@
-import type {FC} from 'react';
+import {type FC, Fragment} from 'react';
 import type {Product} from "@/types/product.interface";
 import {Anchor} from "@ui/Anchor";
 import {ChevronRight, Home} from 'lucide-react';
@@ -9,16 +9,16 @@ interface BreadcrumbProps {
 
 export const ProductBreadcrumb: FC<BreadcrumbProps> = ({product}) => {
     return (
-        <section className="hidden lg:flex gap-2 items-center py-2 px-4 rounded-md w-full bg-muted">
+        <section className="hidden lg:flex gap-2 items-center py-2 px-4 rounded-md w-full bg-muted overflow-x-auto">
             <Anchor href="/">
                 <Home className="w-5 h-5" />
             </Anchor>
             <ChevronRight className="text-primary w-4 h-4" />
             {product.categories.map(category => (
-                <>
+                <Fragment key={category.id}>
                     <Anchor href={`/categories/${category.slug}/`}>{category.name}</Anchor>
                     <ChevronRight className="text-primary opacity-90 w-4 h-4" />
-                </>
+                </Fragment>
             ))}
             <span className="font-medium">{product.name}</span>
         </section>
