fec3bca49d980dff0cf4b2f38ae0c2d5c61e72d2
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-05-25 20:09:07 +0300

    query condition

--- client/src/hooks/useProfile.ts
+++ client/src/hooks/useProfile.ts
@@ -1,9 +1,13 @@
 import {useQuery} from "@tanstack/react-query";
 import UserService from "@api/services/user.service";
+import TokenService from "@api/services/token.service";
 
 export const useProfile = () => {
     const { data } = useQuery(['get profile'], () => {
         return UserService.getProfile();
-    }, { select: ({data}) => data });
+    }, {
+        select: ({data}) => data,
+        enabled: !!TokenService.getToken()
+    });
     return { profile: data };
 }
