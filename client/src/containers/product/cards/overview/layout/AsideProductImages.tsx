b19bfd1540655b26385a426bba917469ee64460e
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-05-29 20:38:35 +0300

    product images design

--- client/src/containers/product/cards/overview/layout/AsideProductImages.tsx
+++ client/src/containers/product/cards/overview/layout/AsideProductImages.tsx
@@ -1,31 +1,41 @@
 import type {FC} from 'react';
 import {AsideProductContainer} from "@containers/product/cards/overview/containers/AsideProductContainer";
 import Image from "next/image";
+import {useState} from "react";
+import {cn} from "@lib/utils";
 
 interface AsideProductImagesProps {
     images: string[];
 }
 
 export const AsideProductImages: FC<AsideProductImagesProps> = ({images}) => {
+    const [mainImageIndex, setMainImageIndex] = useState(0);
+
     return (
         <AsideProductContainer>
             <Image
-                className="sm:w-[520px] md:w-full sm:h-[420px] md:h-auto border rounded-lg object-cover cursor-pointer"
-                src={images[0]}
+                className="w-full h-[300px] sm:w-[520px] sm:h-[420px] md:h-[300px] xl:h-[400px] border rounded-lg object-cover cursor-pointer"
+                src={images[mainImageIndex]}
                 alt={"product image"}
                 width={600}
                 height={600}
             />
 
             <section className="flex flex-card gap-4 my-4 max-h-fit overflow-x-auto">
-                {images.map(src => (
+                {images.map((src, index) => (
                     <Image
-                        className="rounded-lg cursor-pointer border w-[64px] h-[64px] md:w-[96px] md:h-[96px] "
+                        className={cn(
+                            "rounded-lg cursor-pointer border transition-all object-cover",
+                            "w-[64px] h-[64px] md:w-[96px] md:h-[96px] hover:border-foreground", {
+                                'border-foreground shadow-md': mainImageIndex === index
+                            }
+                        )}
+                        onClick={() => setMainImageIndex(index)}
                         key={Math.random() * Date.now()}
                         src={src}
-                        alt={"product image"}
-                        width={90}
-                        height={90}
+                        alt="Product Image"
+                        width={256}
+                        height={256}
                     />
                 ))}
             </section>
