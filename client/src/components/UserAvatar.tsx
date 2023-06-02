e0a58d02be6594790015bc7fe5e956f16d2e5781
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-06-02 20:34:24 +0300

    avatar image covering

--- client/src/components/UserAvatar.tsx
+++ client/src/components/UserAvatar.tsx
@@ -2,13 +2,14 @@ import type {FC} from "react";
 import type {AvatarImageProps} from "@radix-ui/react-avatar";
 import {Skeleton} from "@ui/Skeleton";
 import {Avatar, AvatarFallback, AvatarImage} from "@common/Avatar";
+import {cn} from "@lib/utils";
 
 interface UserAvatarProps extends AvatarImageProps {}
 
-export const UserAvatar: FC<UserAvatarProps> = ({...props}) => {
+export const UserAvatar: FC<UserAvatarProps> = ({className, ...props}) => {
     return (
         <Avatar>
-            <AvatarImage {...props}/>
+            <AvatarImage className={cn("object-cover object-top", className)} {...props}/>
             <AvatarFallback>
                 <Skeleton />
             </AvatarFallback>
