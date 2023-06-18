8521282fb1dd10c7d33529cc8c8f3370fe846d8b
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-06-19 00:18:03 +0300

    hover info card disabled prop

--- client/src/components/HoverInfoCard.tsx
+++ client/src/components/HoverInfoCard.tsx
@@ -4,21 +4,25 @@ import {
     HoverCardContent,
     HoverCardTrigger,
 } from "@common/HoverCard"
+import {HoverCardProps} from "@radix-ui/react-hover-card";
+import {cn} from "@lib/utils";
 
-interface HoverInfoCardProps {
+
+interface HoverInfoCardProps extends HoverCardProps {
     title?: string;
-    description: "string";
+    description: string;
+    disabled?: boolean
 }
 
-export const HoverInfoCard: FC<PropsWithChildren<HoverInfoCardProps>> = ({description, title, children}) => {
+export const HoverInfoCard: FC<PropsWithChildren<HoverInfoCardProps>> = ({description, title, children, disabled, ...props}) => {
     return (
-        <HoverCard openDelay={100} closeDelay={300}>
+        <HoverCard openDelay={100} closeDelay={300} {...props}>
             <HoverCardTrigger asChild>
                 {children}
             </HoverCardTrigger>
-            <HoverCardContent className="w-80">
+            <HoverCardContent className={cn("w-80", disabled && "hidden")}>
                 <h4 className="text-sm font-semibold">{title}</h4>
-                <span className="text-sm text-muted-foreground">
+                <span className="text-sm text-muted-foreground leading-none">
                     {description}
                 </span>
             </HoverCardContent>
