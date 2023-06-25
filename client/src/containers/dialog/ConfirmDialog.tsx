c83bb096d84352f717878a7c9a29ebf9f37b9da7
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2023-06-25 18:20:03 +0300

    confirm dialog component with alert dialog wrapper

--- client/src/containers/dialog/ConfirmDialog.tsx
+++ client/src/containers/dialog/ConfirmDialog.tsx
@@ -1,10 +1,18 @@
 import type {FC, PropsWithChildren} from 'react';
-import type {DialogProps} from "@radix-ui/react-dialog";
-import {Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger} from "@common/Dialog";
-import {Button} from "@ui/Button";
-import {DialogClose} from "@radix-ui/react-dialog";
+import type {AlertDialogProps} from "@radix-ui/react-alert-dialog";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger
+} from "@common/AlertDialog";
 
-interface ConfirmDialog extends DialogProps {
+interface ConfirmDialog extends AlertDialogProps {
     title: string;
     description: string;
     onConfirm: () => void;
@@ -22,34 +30,20 @@ export const ConfirmDialog: FC<PropsWithChildren<ConfirmDialog>> = ({
     ...props
 }) => {
     return (
-        <Dialog open={isOpen} {...props}>
-            <DialogTrigger asChild>
+        <AlertDialog open={isOpen} {...props}>
+            <AlertDialogTrigger asChild>
                 {children}
-            </DialogTrigger>
-            <DialogContent className="h-full sm:h-fit sm:max-w-[425px] flex flex-col justify-center">
-                <DialogHeader>
-                    <DialogTitle className="text-2xl sm:text-md">{title}</DialogTitle>
-                    <DialogDescription>{description}</DialogDescription>
-                </DialogHeader>
-                <section className="flex flex-col md:flex-row gap-2">
-                    <Button
-                        className="flex-1"
-                        onClick={onConfirm}
-                    >
-                        Confirm
-                    </Button>
-                    <DialogClose className="flex-1" asChild>
-                        <Button
-                            type="button"
-                            className="w-full"
-                            variant="destructive"
-                            onClick={onReject}
-                        >
-                            Cancel
-                        </Button>
-                    </DialogClose>
-                    </section>
-            </DialogContent>
-        </Dialog>
+            </AlertDialogTrigger>
+            <AlertDialogContent className="h-full sm:h-fit sm:max-w-[425px] flex flex-col justify-center">
+                <AlertDialogHeader>
+                    <AlertDialogTitle className="text-2xl sm:text-md">{title}</AlertDialogTitle>
+                    <AlertDialogDescription>{description}</AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel onClick={onReject}>Cancel</AlertDialogCancel>
+                    <AlertDialogAction onClick={onConfirm}>Continue</AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
     );
 };
