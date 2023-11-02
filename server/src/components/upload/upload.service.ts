e1c365e24b66395b17fb4b142fffe5c2da198116
Pillow <fagodfagod@gmail.com>
2023-11-02 21:40:12 +0200

    fix issue with context

--- server/src/components/upload/upload.service.ts
+++ server/src/components/upload/upload.service.ts
@@ -13,12 +13,12 @@ const FILE_MAX_SIZE = 1024 * 1024 * 8;
 export class UploadService {
   public async uploadFiles(files: Express.Multer.File[]) {
     const allowFiles = files.filter(this.isFileAllow);
-    return Promise.all(allowFiles.map(this.uploadFile));
+    return Promise.all(allowFiles.map((file) => this.uploadFile(file)));
   }
 
   public async unlinkFiles(fileNames: string[]) {
     const allowFiles = fileNames.filter(this.isFileExist);
-    return Promise.all(allowFiles.map(this.unlinkFile));
+    return Promise.all(allowFiles.map((file) => this.unlinkFile(file)));
   }
 
   public async unlinkFromPaths(filePaths: string[]) {
@@ -45,23 +45,19 @@ export class UploadService {
   private async uploadFile(file: Express.Multer.File) {
     const fileExtension = file.originalname.match(FILE_TYPE_REGEX)[0];
     const fileName = randomBytes(16).toString('hex');
-    const filePath = join(process.cwd(), '/uploads', fileName + fileExtension);
+    const filePath = this.getPath(fileName, fileExtension);
     await writeFile(filePath, file.buffer, 'utf8');
     return { fileName, filePath, fileExtension };
   }
 
   private async unlinkFile(subPaths: string) {
-    return unlink(join(process.cwd(), '/uploads', subPaths));
+    return unlink(this.getPath(subPaths));
   }
 
   private isFileExist(fileName: string) {
-    return existsSync(join(process.cwd(), '/uploads', fileName));
+    return existsSync(this.getPath(fileName));
   }
 
-  /*
-   * Hopefully, I'll figure it out one day.
-   * For some reason, private methods have no context.
-   */
   private getPath(...subPath: string[]) {
     return join(process.cwd(), '/uploads', ...subPath);
   }
